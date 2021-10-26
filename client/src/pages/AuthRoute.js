64573d5c486244091ec95124a4130ca5fe4e5acc
Morgan Segura <morgan@jointhebibleproject.com>
2021-10-26 15:20:30 -0700

    Create AuthRoute component

--- client/src/pages/AuthRoute.js
+++ client/src/pages/AuthRoute.js
@@ -1,7 +1,21 @@
-import React from "react";
-import { Redirect, Route } from "react-router-dom";
-import userStore from "stores/userStore";
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom'
+import userStore from 'stores/userStore'
 
-export default function AuthRoute() {
-  return <Route />;
+export default function AuthRoute({ conponent: Component, ...rest }) {
+	const current = userStore(state => state.current)
+	const storage = JSON.parse(localStorage.getItem('user-storage'))
+
+	return (
+		<Route
+			{...rest}
+			render={props =>
+				current || storage?.state?.current ? (
+					<Component {...rest} />
+				) : (
+					<Redirect to="/login" />
+				)
+			}
+		/>
+	)
 }
